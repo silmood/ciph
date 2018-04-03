052877bd85e8577eb43db4563d969476d2ca69f0
Pedro Hernández <dsilmood@gmail.com>
2018-04-02 19:12:00 -0500

    First real UI test was written

--- app/src/androidTest/java/com/silmood/ciph/CipherTest.java
+++ app/src/androidTest/java/com/silmood/ciph/CipherTest.java
@@ -1,5 +1,6 @@
 package com.silmood.ciph;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -7,10 +8,16 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -21,7 +28,14 @@ public class CipherTest {
             new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void cipherSayHelloWorld() {
-        onView(withText("Hello World!")).check(matches(isDisplayed()));
+    public void cipherRot13SimpleText() {
+        // Type Message to cipher
+        onView(withHint("Message")).perform(typeText("Hello World"));
+
+        //Click cipher button
+        onView(withText("CIPHER")).perform(click());
+
+        //Check Rot 13 output
+        onView(withText("Uryyb Jbeyq")).check(matches(isDisplayed()));
     }
 }
