fcb707769e9e212c9e51f305eb8f7b667fa03055
Pedro Hernández <dsilmood@gmail.com>
2018-04-04 07:45:56 -0500

    🚀 rot13 was implemented

--- app/src/main/java/com/silmood/ciph/Cipher.java
+++ app/src/main/java/com/silmood/ciph/Cipher.java
@@ -3,7 +3,18 @@ package com.silmood.ciph;
 
 public class Cipher {
 
-    public String rot13(String text){
-        return null;
+    public String rot13(String text) {
+        char[] chars = text.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : chars) {
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+
+            result.append(c);
+        }
+        return result.toString();
     }
 }
